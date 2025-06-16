33824ef527da2559f233af3f8654b65de8442207
Endrisbits <mendri94@gmail.com>
2025-06-16 08:34:15 +0200

    feat: Adds proper Unmarshalling of the JSON response from the API

--- backend_app/app.go
+++ backend_app/app.go
@@ -4,7 +4,13 @@ import (
     "fmt"
     "net/http"
     "io"
+    "encoding/json"
 )
+
+type ApiResponse struct {
+    Message string
+    Data map[string]string
+}
 
 var apiEndpoint = "http://chatapi.freshdesk.intranet.pflanzmich.de:8000/"
 
@@ -24,27 +30,32 @@ func orderHandler (w http.ResponseWriter, req *http.Request) {
     if err != nil {
       fmt.Fprintf(w, "An error occured while processing request: %s", err)
     } else {
-        fmt.Fprintf(w, "%s %s", order_id, response["id"], response["body"])
+        fmt.Fprintf(w, "Message: %s\n", response.Message)
+        fmt.Fprintf(w, "Data:\n") 
+        for key, val := range response.Data {
+            fmt.Fprintf(w, "%s: %s \n", key, val)
+        }
     }
     
 }
 
-func apiStatusCheck (id string) (map[string]string, error){
+func apiStatusCheck (id string) (ApiResponse, error){
+    var respJSON ApiResponse = ApiResponse{}
     resp, err := http.Get(fmt.Sprintf("%sstatusabfrage?orderId=%s",apiEndpoint, id))
     if err != nil {
-      return nil, err
+      return respJSON, err
     }
 
     body, err := io.ReadAll(resp.Body)
     if err != nil {
-        return nil, err
+        return respJSON, err
     }
-    //Convert the body to type string
-    sb := string(body)
-    response := make(map[string]string)
-    response["id"] = id
-    response["body"] = sb
-    return response, nil
+    //Umarshalling the response to JSON struct
+    err = json.Unmarshal(body, &respJSON) 
+    if err != nil {
+        return respJSON, err
+    }
+    return respJSON, nil
 }
 
 func main() {
